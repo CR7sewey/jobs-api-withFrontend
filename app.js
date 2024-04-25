118c55b97b15492534eaf2a341ca209af9c35d66
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-25 16:17:04 +0100

    Cleaning app

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ require("express-async-errors");
 
 // extra security packages
 const helmet = require("helmet");
-const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
 
 const express = require("express");
 // DB CONNECTION
@@ -24,15 +22,8 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
-app.search("trust proxy", 1);
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 60, //
-    max: 100, //max request (100 reequest per windowMs)
-  })
-);
+
 // extra packages
 
 // routes
