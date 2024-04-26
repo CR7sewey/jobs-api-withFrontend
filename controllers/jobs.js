95446a7ff5c29bf1e1a1d96dcec200d0e2dc8f9d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-26 21:27:33 +0100

    Sending vars to frontend for pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -25,9 +25,21 @@ const getAllJobs = async (req, res) => {
     "z-a": { position: -1 },
   };
   console.log(sortType[sort]);
-  const jobs = await Job.find({ ...queryObj }).sort(sortType[sort]);
+  let jobs = Job.find({ ...queryObj }).sort(sortType[sort]);
 
-  return res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  jobs = jobs.skip(skip).limit(limit);
+
+  jobs = await jobs;
+
+  // to send to the frontend (for pagination!)
+  const totalJobs = await Job.countDocuments(queryObj); // coutn number of jobs with query obj (Similar to find(queryObj).length())
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  return res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getJob = async (req, res) => {
