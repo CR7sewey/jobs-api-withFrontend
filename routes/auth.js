9b33ea6711eae9813dfe83997b0446fcd464705d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-27 10:45:37 +0100

    API limiter - on login and register

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,17 @@ const { login, register, updateUser } = require("../controllers/auth");
 const tokenVerification = require("../middleware/authentication");
 const testUserMiddleware = require("../middleware/test-user");
 
-router.post("/register", register);
-router.post("/login", login);
+const rateLimiter = require("express-rate-limit");
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: {
+    msg: "To many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
 router.patch("/updateUser", tokenVerification, testUserMiddleware, updateUser);
 
 module.exports = router;
